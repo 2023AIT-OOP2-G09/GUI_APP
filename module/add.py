4f28dc0ad70961776f179e2ed41ee40b7b99639b
ShibataToa <153697415+ShibataToa@users.noreply.github.com>
2024-01-11 15:45:44 +0900

    otamesi
    修正のやり方

--- module/add.py
+++ module/add.py
@@ -1,55 +1,68 @@
 from flask import Flask, request, render_template, jsonify
-import json 
+import json
 
 app = Flask(__name__)
-@app.route('/address', methods=["GET"])
+
+
+@app.route("/address", methods=["GET"])
 def address_get():
-
     # 検索パラメータの取得
-    p_date = request.args.get('dt', None)
-    p_schedule= request.args.get('sh', None)
-    p_time = request.args.get('tm', None)
-   
+    p_date = request.args.get("dt", None)
+    p_schedule = request.args.get("sh", None)
+    p_time = request.args.get("tm", None)
 
-    with open('address.json') as f:
+    with open("schedule.json") as f:
         json_data = json.load(f)
 
     # パラメータにより返すデータをフィルタリングする
     if p_date is not None:
-        json_data = list(filter(lambda item: p_date.lower() in item["date"].lower(), json_data))
+        json_data = list(
+            filter(lambda item: p_date.lower() in item["date"].lower(), json_data)
+        )
     if p_schedule is not None:
         # ラムダにより匿名関数化しているため、分かりづらく見えますが、filter関数には関数を渡す必要があるため、
         # json_dataの中からfirst_name内にパラメータの値が含まれているかどうかを判定する関数を匿名で作成し、
         # それによって得た結果のデータを基にlist生成しています。
-        json_data = list(filter(lambda item: p_schedule.lower() in item["schedule"].lower(), json_data))
+        json_data = list(
+            filter(
+                lambda item: p_schedule.lower() in item["schedule"].lower(), json_data
+            )
+        )
     if p_time is not None:
-        json_data = list(filter(lambda item: p_time.lower() in item["time"].lower(), json_data))
-   
+        json_data = list(
+            filter(lambda item: p_time.lower() in item["time"].lower(), json_data)
+        )
 
     return jsonify(json_data)
 
-@app.route('/address', methods=["POST"])
+
+@app.route("/address", methods=["POST"])
 def address_post():
     try:
         # リクエストからデータを取得
         data = request.form.to_dict()
 
         # 既存データを読み込む
-        with open('schedule.json', 'r') as f:
+        with open("schedule.json", "r") as f:
             existing_data = json.load(f)
 
         # 新しいデータを追加
         existing_data.append(data)
 
         # データをファイルに書き込む
-        with open('schedule.json', 'w') as f:
+        with open("schedule.json", "w") as f:
             json.dump(existing_data, f, indent=2, ensure_ascii=False)
 
-        return jsonify({"message": "Data added successfully!"})
+        return jsonify({"message": "読み込み成功"})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
 # http://127.0.0.1:5000/
-@app.route('/')
+@app.route("/")
 def index():
     return render_template("addressbook.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
