3b2f1d3c3e196a3d8cb531cdb8db8e1ea33e4ba0
shiruku-shibata <k22065kk@aitech.ac.jp>
2024-01-17 10:39:49 +0900

    できている部分の連結
    できている部分を連結しました。
    一箇所エラーありです。
    担当に連絡済みなので、対処待ち

---
+++ main.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request, jsonify
+import json
+from datetime import datetime
+
+from module.add import schedule_duplicate
+
+app = Flask(__name__)
+
+# 最初のカレンダーのページ
+@app.route('/')
+def index():
+    # ここにカレンダー表示のtemplateを返す
+    return render_template('index.html')
+
+# 予定追加のページ
+@app.route('/add')
+def add():
+    # 予定追加のtemplateを返す
+    return render_template('AddSchedule.html')
+
+# データ追加工程
+@app.route("/add/to", methods=["GET", "POST"])
+def address_get():
+    if request.method == "POST":
+        try:
+            # 検索パラメータの取得
+            p_date = request.form.get("date")
+            print(p_date)
+            p_schedule = request.form.get("schedule")
+            # p_time = request.args.get("tm")
+            # データを分ける
+            p_date_obje = datetime.strptime(p_date, "%Y/%m/%d").date()
+            # 新しいデータの箱を作成
+            new_schedule = {
+                "date": p_date_obje.strftime("%Y/%m/%d"),
+                "schedule": p_schedule,
+                "time": "0:00",  # この時間はとりあえず初期値
+            }
+            # 既存データ読み込み
+            with open("schedule.json") as f:
+                existing_data = json.load(f)
+            # 重複チェック
+            if schedule_duplicate(new_schedule, existing_data):
+                return jsonify({"message": "同じ予定が既にあります"})
+            # 既存データに新しい予定を’追加
+            existing_data.append(new_schedule)
+            # データをファイルに書き込む
+            with open("schedule.json", "w") as f:
+                json.dump(existing_data, f, indent=2, ensure_ascii=False)
+
+            return jsonify({"message": "予定追加"})
+        except Exception as e:
+            return jsonify({"message": str(e)})
+
+    return render_template("add.html")
+
+# Flaskアプリケーションの起動
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False)
